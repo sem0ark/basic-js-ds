fe923d5db063235c9e3fabb0d86e68829e1994d3
sem0ark <arcadii.sem@gmail.com>
2023-12-14 16:59:12 +0100

    feat: complete stack

--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.a = [];
+  }
+
+  push(element) {
+    this.a.push(element);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.a.length) return undefined;
+    return this.a.pop();
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.a.length) return undefined;
+    return this.a[this.a.length - 1];
   }
 }
 
